366cd18d9f84c06bfe7399257e99620122042022
shadow-hacrr <shadowofficial887766@gmail.com>
2026-04-02 23:11:52 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+    mainToken: '8627860370:AAFGp8sgzCF756mrYxI5I_q45UWjReghLeU', // Bot Token Here 
+    
+    S7: 'SHDOW', // Owner, Name
+    
+    adminId: '8627624927', // Owner Chat ID ( control /address, /delresell )
+    
+    channel: 'https://t.me/ssbugchannel', // TG channel link
+    
+    group: 'https://t.me/+BrKnLeHRciczZWRk', // TG group link
+    
+    bot: 'SHADOW', // Bot name 
+    
+    logo: './SY/Loves.jpg' // Bot logo path or URL
+};
